e1fd9c36653f5bfba033a352d71cd453b0c5a850
lidingyu <lidingyu@sohu.com>
2024-06-22 16:11:37 +0800

    add hdiffpatch plugin

--- Assets/Scripts/PatchingTest.cs
+++ Assets/Scripts/PatchingTest.cs
@@ -1,7 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using UnityEngine;
+using BsDiff;
+using IngameDebugConsole;
+using Debug = UnityEngine.Debug;
 
 public class PatchingTest : MonoBehaviour
 {
@@ -9,8 +14,37 @@ public class PatchingTest : MonoBehaviour
     private string DstPath => Path.Combine(Application.persistentDataPath, "Dst");
     private string PatchPath => Path.Combine(Application.persistentDataPath, "Patch");
 
-    void ExcuteTest()
+    private void Start()
     {
-        
+        DebugLogConsole.AddCommand("patch", "bsdiff patch file", BsdiffPatchFile);
+    }
+
+    void BsdiffPatchFile()
+    {
+        var srcFiles = Directory.GetFiles(SrcPath, "*.*", SearchOption.AllDirectories);
+        var sw = new Stopwatch();
+        foreach (var file in srcFiles)
+        {
+            var relativePath = Path.GetRelativePath(SrcPath, file);
+            var relativeDir = Path.GetDirectoryName(relativePath);
+            var fileName = Path.GetFileName(file);
+            if (!Directory.Exists(Path.Combine(DstPath, relativeDir)))
+            {
+                Directory.CreateDirectory(Path.Combine(DstPath, relativeDir));
+            }
+            
+            if (File.Exists(Path.Combine(PatchPath, relativePath + ".patch")))
+            {
+                Debug.Log($"start patching {fileName}");
+                using (var srcFs = File.OpenRead(file))
+                using (var dstFs = File.OpenWrite(Path.Combine(DstPath, relativePath)))
+                {
+                    sw.Restart();
+                    BinaryPatchUtility.Apply(srcFs, () => File.OpenRead(Path.Combine(PatchPath, relativePath + ".patch")), dstFs);
+                    sw.Stop();
+                    Debug.Log($"{fileName} patched in {sw.ElapsedMilliseconds}ms");
+                }
+            }
+        }
     }
 }
